d885ee87b0454c4609d965284e14e015f3215363
M. Husic <127348494+HMByteSensei@users.noreply.github.com>
2025-03-23 22:06:00 +0100

    Kth grammar symbol

---
+++ K-thSymbolInGrammar.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int kthGrammar(int n, int k) {
+        if (n == 1) {
+            return 0;
+        }
+        int length = 1 << (n - 1); // 2^(n-1)
+        if (k <= length / 2) {
+            return kthGrammar(n - 1, k);
+        } else {
+            return !kthGrammar(n - 1, k - length / 2);
+        }
+    }
+};
